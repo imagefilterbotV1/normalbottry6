b418ffd2a09e341e4328892303ca5cffb4b84657
imagefilterbotV1 <96852831+imagefilterbotV1@users.noreply.github.com>
2022-01-04 21:11:15 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -4,21 +4,20 @@
 
 class Translation(object):
     
-    START_TEXT = """<b>Hey {}!!</b>
-<i>Am Just A Advance Auto Filter Bot....😉
+    START_TEXT = """<b>Hey 📺{}!!</b>
+<i> 🙂 HAI  AM ⭕ ZEUS ⭕ 
 
-Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
+♻️ JOIN OUR GROUP ♻️
 
-For More Details Click Help Button Below..
-@CrazyBotsz
+➡️ @D_W_T_1
 </i>"""    
     
     HELP_TEXT = """
-<b><i><u>How To Use Me!?</u></i></b>
+<b><i><u>😌 How To Use Me!?</u></i></b>
 
 <i>
--> Add Me To Any Group And Make Me Admin
--> Add Me To Your Desired Channel
+-> ♻️ JOIN OUR GROUP 
+-> ♻️ ASK FOR YOUR MOVIES 🍿 WITH CORRECT ✅ SPELLING 🙂
 </i>
 
 <b>Bot Commands (Works Only In Groups) :</b>
@@ -43,16 +42,16 @@ For More Details Click Help Button Below..
             
             -> <code>Status</code> - Button Will Shows The Stats Of Your Channel
             
-@CrazyBotsz
+@D_W_T_1
 """
     
-    ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
+    ABOUT_TEXT = """<b>➥ Name</b> : <code> 🍿 ZEUS 🍿</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Willing1234">🙂 APPU KUTTEN 🙂</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
-<b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>➥ Library</b> : <i><a href="https://t.me/joinchat/BhYlk3vvhG5hMmZl">♻️ JOIN US ♻️</a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://t.me/joinchat/BhYlk3vvhG5hMmZl">Click Me</a></i>
 """
